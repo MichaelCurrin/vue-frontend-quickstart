3851ea4e9105c957ede5a7ee888a20ab3887dc8c
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-04-05 20:28:45 +0200

    refactor: Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 import { createApp } from "https://unpkg.com/vue@3.0.7/dist/vue.esm-browser.js";
 
+const template = `
+<p>Hello, {{ name }}!</p>
+`
+
 const app = createApp({
   data() {
     return {
       name: "developer",
     };
   },
-  template: `
-    <p>Hello, {{ name }}!</p>
-  `,
+  template,
 });
 
 app.mount("#app");
